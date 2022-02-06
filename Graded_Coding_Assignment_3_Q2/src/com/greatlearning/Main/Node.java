0ee39a03dd45bf5402888d0ee9cde0da4738cb77
ABHISHEK DIXIT <srpl.abhishekdixit@gmail.com>
2022-02-06 23:31:33 +0530

    Commit

---
+++ Graded_Coding_Assignment_3_Q2/src/com/greatlearning/Main/Node.java
@@ -0,0 +1,14 @@
+package com.greatlearning.Main;
+
+public class Node {
+
+	int data;
+	
+	public Node left;
+	public Node right;
+
+	Node(int L) {
+		this.data = L;
+		this.left = this.right = null;
+	}
+}
